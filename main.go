f63b74e7c7d479e6a839026bbce0dc95f4b875a5
Leon Hwang <leon.hwang@linux.dev>
2025-03-02 19:53:10 +0800

    bpf: Use header files from libbpf
    Use the latest bpf header files from
    [libbpf](https://github.com/libbpf/libbpf). Then, it's unnecessary to
    keep these header files in this repo.
    
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ import (
 	"github.com/leonhwangprojects/btrace/internal/btrace"
 )
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang btrace ./bpf/btrace.c -- -g -D__TARGET_ARCH_x86 -I./bpf/headers -Wall
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang feat ./bpf/feature.c -- -g -D__TARGET_ARCH_x86 -I./bpf/headers -Wall
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang btrace ./bpf/btrace.c -- -g -D__TARGET_ARCH_x86 -I./bpf/headers -I./lib/libbpf/src -Wall
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang feat ./bpf/feature.c -- -g -D__TARGET_ARCH_x86 -I./bpf/headers -I./lib/libbpf/src -Wall
 
 func main() {
 	flags, err := btrace.ParseFlags()
