uni7corn/bpfsnoop

commit dd42c0bdf772c7ad5976621252fafff367855510

bpfsnoop: Run both entry and exit mode
As for `-p` and `-k`, it will be useful to trace the target(s) with
both fentry and fexit.

e.g.

```bash
$ sudo ./bpfsnoop -k 'icmp_rcv' --filter-pkt 'host 1.1.1.1' --output-pkt --output-arg 'skb->dev->name' -m 'entry,exit'
2025/06/18 16:25:09 bpfsnoop is running..
icmp_rcv[en] args=((struct sk_buff *)skb=0xffff8f75c8efd300) cpu=6 process=(0:swapper/6)
Pkt tuple: 1.1.1.1 -> 192.168.241.133 (ICMP)
Arg attrs: (array(char[16]))'skb->dev->name'="ens33"

icmp_rcv[ex] args=((struct sk_buff *)skb=0xffff8f75c8efd300) retval=(int)0 cpu=6 process=(0:swapper/6)
Pkt tuple: 1.1.1.1 -> 192.168.241.133 (ICMP)
Arg attrs: (array(char[16]))'skb->dev->name'="ens33"
```

Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

internal/bpfsnoop/bpf_tracing.go
@@ -62,14 +62,22 @@ func NewBPFTracing(spec, insnSpec *ebpf.CollectionSpec, reusedMaps map[string]*e
 	var errg errgroup.Group
 
 	for _, info := range bprogs.tracings {
+		bothEntryExit := hasModeEntry() && hasModeExit()
 		info := info
+
 		errg.Go(func() error {
-			return t.traceProg(spec, reusedMaps, info, bprogs)
+			return t.traceProg(spec, reusedMaps, info, bprogs, hasModeExit())
 		})
+
+		if bothEntryExit {
+			errg.Go(func() error {
+				return t.traceProg(spec, reusedMaps, info, bprogs, !hasModeExit())
+			})
+		}
 	}
 
 	for _, fn := range kfuncs {
-		bothEntryExit := fn.Insn
+		bothEntryExit := fn.Insn || (hasModeEntry() && hasModeExit())
 		fn := fn
 
 		if fn.IsTp {
@@ -80,12 +88,12 @@ func NewBPFTracing(spec, insnSpec *ebpf.CollectionSpec, reusedMaps map[string]*e
 		}
 
 		errg.Go(func() error {
-			return t.traceFunc(spec, reusedMaps, fn, bothEntryExit, mode == TracingModeExit)
+			return t.traceFunc(spec, reusedMaps, fn, bothEntryExit, hasModeExit())
 		})
 
 		if bothEntryExit {
 			errg.Go(func() error {
-				return t.traceFunc(spec, reusedMaps, fn, bothEntryExit, mode != TracingModeExit)
+				return t.traceFunc(spec, reusedMaps, fn, bothEntryExit, !hasModeExit())
 			})
 		}
 	}
@@ -298,7 +306,7 @@ func (t *bpfTracing) injectPktOutput(prog *ebpf.ProgramSpec, params []btf.FuncPa
 	return false
 }
 
-func (t *bpfTracing) traceProg(spec *ebpf.CollectionSpec, reusedMaps map[string]*ebpf.Map, info bpfTracingInfo, bprogs *bpfProgs) error {
+func (t *bpfTracing) traceProg(spec *ebpf.CollectionSpec, reusedMaps map[string]*ebpf.Map, info bpfTracingInfo, bprogs *bpfProgs, fexit bool) error {
 	spec = spec.Copy()
 
 	traceeName := info.fn.Name
@@ -318,18 +326,18 @@ func (t *bpfTracing) traceProg(spec *ebpf.CollectionSpec, reusedMaps map[string]
 	}
 	bprogs.funcs[info.funcIP].funcArgs = args
 	bprogs.funcs[info.funcIP].argDataSz = argDataSize
-	fnArgsBufSize, err := injectOutputFuncArgs(progSpec, info.params, info.ret, mode == TracingModeExit)
+	fnArgsBufSize, err := injectOutputFuncArgs(progSpec, info.params, info.ret, fexit)
 	if err != nil {
 		return fmt.Errorf("failed to inject output func args: %w", err)
 	}
 	bprogs.funcs[info.funcIP].argsBufSz = fnArgsBufSize
 
-	if err := setBpfsnoopConfig(spec, uint64(info.funcIP), len(info.params), fnArgsBufSize, argDataSize, false, mode == TracingModeExit); err != nil {
+	if err := setBpfsnoopConfig(spec, uint64(info.funcIP), len(info.params), fnArgsBufSize, argDataSize, false, fexit); err != nil {
 		return fmt.Errorf("failed to set bpfsnoop config: %w", err)
 	}
 
 	attachType := ebpf.AttachTraceFExit
-	if mode == TracingModeEntry {
+	if !fexit {
 		attachType = ebpf.AttachTraceFEntry
 	}
 

internal/bpfsnoop/bpf_tail_call.go
@@ -144,10 +144,6 @@ func ProbeTailcallIssue(spec, tailcallSpec, readSpec *ebpf.CollectionSpec) error
 	clearFilterArgSubprog(prog)
 
 	attachType := ebpf.AttachTraceFExit
-	if mode == TracingModeEntry {
-		attachType = ebpf.AttachTraceFEntry
-	}
-
 	prog.AttachTarget = tcProg
 	prog.AttachTo = tcProgName
 	prog.AttachType = attachType

internal/bpfsnoop/bpfsnoop.go
@@ -122,11 +122,18 @@ func Run(reader *ringbuf.Reader, helpers *Helpers, maps map[string]*ebpf.Map, w 
 			}
 		}
 
+		fnName := fnInfo.name
+		if event.Type == eventTypeFuncExit {
+			fnName += "[ex]"
+		} else if event.Type == eventTypeFuncEntry && !fnInfo.isTp {
+			fnName += "[en]"
+		}
+
 		if colorfulOutput {
-			color.New(color.FgYellow, color.Bold).Fprint(&sb, fnInfo.name, " ")
+			color.New(color.FgYellow, color.Bold).Fprint(&sb, fnName, " ")
 			color.New(color.FgBlue).Fprintf(&sb, "args")
 		} else {
-			fmt.Fprint(&sb, fnInfo.name, " args")
+			fmt.Fprint(&sb, fnName, " args")
 		}
 
 		withRetval := event.Type == eventTypeFuncExit

internal/bpfsnoop/bpfsnoop_lbr.go
@@ -37,7 +37,7 @@ func (s *lbrStack) get(funcIP uintptr, lbrData *LbrData, helpers *Helpers) bool 
 					break
 				}
 			}
-		} else if mode == TracingModeExit {
+		} else if hasModeExit() {
 			for i := range entries {
 				if progInfo.contains(entries[i].From) || progInfo.contains(entries[i].To) {
 					entries = entries[i:]

internal/bpfsnoop/flags.go
@@ -4,8 +4,10 @@
 package bpfsnoop
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 
 	flag "github.com/spf13/pflag"
 )
@@ -19,7 +21,7 @@ var (
 	verbose           bool
 	debugLog          bool
 	disasmIntelSyntax bool
-	mode              string
+	modes             []string
 	filterArg         []string
 	filterPkt         string
 	outputArg         []string
@@ -72,7 +74,7 @@ func ParseFlags() (*Flags, error) {
 	f.BoolVar(&disasmIntelSyntax, "disasm-intel-syntax", false, "use Intel asm syntax for disasm, ATT asm syntax by default")
 	f.BoolVarP(&verbose, "verbose", "v", false, "output verbose log")
 	f.BoolVarP(&debugLog, "debug-log", "D", false, "output many debug logs")
-	f.StringVarP(&mode, "mode", "m", TracingModeExit, "mode of bpfsnoop, exit or entry")
+	f.StringSliceVarP(&modes, "mode", "m", []string{TracingModeExit}, "mode of bpfsnoop, exit and/or entry")
 	f.BoolVar(&outputLbr, "output-lbr", false, "output LBR perf event")
 	f.BoolVar(&outputFuncStack, "output-stack", false, "output function call stack")
 	f.StringVar(&outputFlameGraph, "output-flamegraph", "", "output flamegraph fold data")
@@ -115,11 +117,44 @@ func ParseFlags() (*Flags, error) {
 		}
 	}
 
+	if e := flags.checkMode(); e != nil {
+		return nil, e
+	}
+
 	return &flags, err
 }
 
 func (f *Flags) ParseProgs() ([]ProgFlag, error) {
 	return parseProgsFlag(f.progs)
+}
+
+func (f *Flags) checkMode() error {
+	switch len(modes) {
+	case 1:
+		if !slices.Contains([]string{TracingModeEntry, TracingModeExit}, modes[0]) {
+			return fmt.Errorf("invalid mode %q, must be %q and/or %q", modes[0], TracingModeEntry, TracingModeExit)
+		}
+
+	case 2:
+		for _, m := range modes {
+			if !slices.Contains([]string{TracingModeEntry, TracingModeExit}, m) {
+				return fmt.Errorf("invalid mode %q, must be %q and/or %q", m, TracingModeEntry, TracingModeExit)
+			}
+		}
+
+	default:
+		return fmt.Errorf("invalid number of modes %d, must be 1 or 2", len(modes))
+	}
+
+	return nil
+}
+
+func hasModeEntry() bool {
+	return slices.Contains(modes, TracingModeEntry)
+}
+
+func hasModeExit() bool {
+	return slices.Contains(modes, TracingModeExit)
 }
 
 func (f *Flags) Kfuncs() []string {
@@ -142,24 +177,12 @@ func (f *Flags) Disasm() bool {
 	return f.disasm
 }
 
-func (f *Flags) Mode() string {
-	return mode
-}
-
 func (f *Flags) OutputLbr() bool {
 	return outputLbr
 }
 
 func (f *Flags) OutputFuncStack() bool {
 	return outputFuncStack
-}
-
-func (f *Flags) OtherMode() string {
-	if mode == TracingModeExit {
-		return TracingModeEntry
-	}
-
-	return TracingModeExit
 }
 
 func (f *Flags) ShowFuncProto() bool {

internal/bpfsnoop/kernel_functions.go
@@ -165,7 +165,7 @@ func searchKernelFuncs(funcs, kmods []string, ksyms *Kallsyms, maxArgs int) (KFu
 		return nil, err
 	}
 
-	if mode != TracingModeExit {
+	if !hasModeExit() {
 		return kfuncs, nil
 	}
 

internal/bpfsnoop/kernel_functions_max_arg.go
@@ -36,10 +36,6 @@ func DetectSupportedMaxArg(traceableSpec, spec *ebpf.CollectionSpec, ksyms *Kall
 	clearFilterArgSubprog(prog)
 
 	attachType := ebpf.AttachTraceFExit
-	if mode == TracingModeEntry {
-		attachType = ebpf.AttachTraceFEntry
-	}
-
 	kfunc := maps.Values(kfuncs)[0]
 	prog.AttachTo = kfunc.Ksym.name
 	prog.AttachType = attachType

main.go
@@ -6,11 +6,9 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"slices"
 	"syscall"
 	"time"
 
@@ -46,10 +44,6 @@ func main() {
 		bpfsnoop.ShowFuncProto(flags, tpSpec, tpModSpec)
 		return
 	}
-
-	mode := flags.Mode()
-	assert.True(slices.Contains([]string{bpfsnoop.TracingModeEntry, bpfsnoop.TracingModeExit}, mode),
-		fmt.Sprintf("Mode (%s) must be exit or entry", mode))
 
 	progs, err := flags.ParseProgs()
 	assert.NoErr(err, "Failed to parse bpf prog infos: %v")

commit ddbe225a0424c60277415d1ce1e3ac1b4797d2b6

bpfsnoop: Fix running both entry and exit mode for tracing bpf prog
The issue was introduced by commit dd42c0bdf772 ("bpfsnoop: Run both entry and exit mode").

It is a code logic issue that missed setting `bothEntryExit` to bpf cfg.

Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

internal/bpfsnoop/bpf_tracing.go
@@ -66,12 +66,12 @@ func NewBPFTracing(spec, insnSpec *ebpf.CollectionSpec, reusedMaps map[string]*e
 		info := info
 
 		errg.Go(func() error {
-			return t.traceProg(spec, reusedMaps, info, bprogs, hasModeExit())
+			return t.traceProg(spec, reusedMaps, info, bprogs, bothEntryExit, hasModeExit())
 		})
 
 		if bothEntryExit {
 			errg.Go(func() error {
-				return t.traceProg(spec, reusedMaps, info, bprogs, !hasModeExit())
+				return t.traceProg(spec, reusedMaps, info, bprogs, bothEntryExit, !hasModeExit())
 			})
 		}
 	}
@@ -322,7 +322,7 @@ func (t *bpfTracing) injectPktOutput(prog *ebpf.ProgramSpec, params []btf.FuncPa
 	return false
 }
 
-func (t *bpfTracing) traceProg(spec *ebpf.CollectionSpec, reusedMaps map[string]*ebpf.Map, info bpfTracingInfo, bprogs *bpfProgs, fexit bool) error {
+func (t *bpfTracing) traceProg(spec *ebpf.CollectionSpec, reusedMaps map[string]*ebpf.Map, info bpfTracingInfo, bprogs *bpfProgs, bothEntryExit, fexit bool) error {
 	spec = spec.Copy()
 
 	traceeName := info.fn.Name
@@ -352,7 +352,7 @@ func (t *bpfTracing) traceProg(spec *ebpf.CollectionSpec, reusedMaps map[string]
 		bprogs.funcs[info.funcIP].argEntrySz = fnArgsBufSize
 	}
 
-	if err := setBpfsnoopConfig(spec, uint64(info.funcIP), len(info.params), fnArgsBufSize, argDataSize, false, fexit); err != nil {
+	if err := setBpfsnoopConfig(spec, uint64(info.funcIP), len(info.params), fnArgsBufSize, argDataSize, bothEntryExit, fexit); err != nil {
 		return fmt.Errorf("failed to set bpfsnoop config: %w", err)
 	}
 