uni7corn/bpfsnoop

commit 01c5cf6dfa7032aaf0c36b0f870ccb908b7fb956

fngraph: Fix for trampoline jmp mode on x86
Since commit 373f2f44c300 ("bpf,x86: adjust the "jmp" mode for bpf trampoline")
on 6.19 kernel, it is unable to get the tracee's IP via stack.

To adapt it, map the call-return addresses of trampoline to the tracee's
IP when jmp mode is enabled on x86.

Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

bpf/traceable.c
@@ -10,26 +10,32 @@
 volatile const u64 addrs[ADDR_CAP];
 volatile const u32 nr_addrs SEC(".rodata.nr_addrs");
 volatile const u32 has_endbr SEC(".rodata.endbr") = 0;
+volatile const u32 tramp_jmp;
+u64 tramps[ADDR_CAP] SEC(".data.tramps");
 bool traceables[ADDR_CAP];
 bool run SEC(".data.run");
 
-static __noinline bool
-is_traceable(u64 addr)
+static __noinline void
+probe_addr_info(int i)
 {
+    u64 addr = addrs[i];
     u8 buff[16], *ptr;
+    bool traceable;
 
     if (bpf_probe_read_kernel(&buff, 16, (void *) addr))
-        return false;
+        return;
 
 #if defined(bpf_target_x86)
     static const u64 nop5 = 0x0000441F0F;
     u64 a, b;
 
     ptr = has_endbr ? buff + 4 : buff;
+    if (tramp_jmp && ptr[0] == 0xE9 /* jmp */)
+        tramps[i] = addr + (has_endbr ? 4 : 0) + 5 + (__s32) (((*(u64 *) ptr) >> 8) & 0xFFFFFFFF);
     /* Avoid 'misaligned stack access off 0+-12+0 size 8' */
     a = *(u32 *) ptr;
     b = *(u8 *) (ptr + 4);
-    return (b<<32 | a) == nop5 /* nop5 */ || ptr[0] == 0xE8 /* callq */ ||
+    traceable = (b<<32 | a) == nop5 /* nop5 */ || ptr[0] == 0xE8 /* callq */ ||
            /* 373f2f44c300 ("bpf,x86: adjust the "jmp" mode for bpf trampoline")
             * since 6.19 kernel.
             */
@@ -39,12 +45,14 @@ is_traceable(u64 addr)
     ptr = buff + 4;
     u32 insn = *(u32 *) ptr;
 
-    return insn == 0xD503201F /* nop */ || ptr[3] == 0x97 /* bl */ ||
+    traceable = insn == 0xD503201F /* nop */ || ptr[3] == 0x97 /* bl */ ||
            ptr[3] == 0x94 /* blr */;
 
 #else
 # error "Unsupported architecture"
 #endif
+
+    traceables[i] = traceable;
 }
 
 SEC("fentry/__x64_sys_nanosleep")
@@ -57,7 +65,7 @@ int BPF_PROG(detect, struct pt_regs *regs)
     for (int i = 0; i < ADDR_CAP; i++) {
         if (i >= nr_addrs)
             break;
-        traceables[i] = is_traceable(addrs[i]);
+        probe_addr_info(i);
     }
 
     return BPF_OK;

bpf/bpfsnoop_fgraph.c
@@ -119,18 +119,15 @@ try_get_session(void *ctx, int *depth, __u64 pid_tgid)
         if (!func_ip)
             continue;
 
-        if (!after_current && func_ip != cfg->func_ip)
-            continue;
+        if (after_current) {
+            session_id = get_session(pid_tgid, func_ip);
+            if (session_id)
+                return session_id;
 
-        session_id = get_session(pid_tgid, func_ip);
-        if (session_id)
-            return session_id;
-
-        if (!after_current) {
-            after_current = true;
-        } else {
             if (++(*depth) > cfg->max_depth)
                 return 0;
+        } else if (func_ip == cfg->func_ip) {
+            after_current = true;
         }
     }
 

internal/bpfsnoop/bpf_feature.go
@@ -126,7 +126,7 @@ func btfEnumValue(enum, value string) (bool, error) {
 }
 
 func haveTrampolineJmpMode(insns []byte) {
-	if len(insns) < 9 {
+	if !onAmd64 || len(insns) < 9 {
 		return
 	}
 
@@ -136,4 +136,5 @@ func haveTrampolineJmpMode(insns []byte) {
 	}
 
 	trampJmpMode = insns[0] == 0xE9 /* jmp rel32 */
+	DebugLog("Trampoline jmp mode: %v", trampJmpMode)
 }

internal/bpfsnoop/bpf_kfuncs.go
@@ -26,7 +26,7 @@ func b2i(b bool) int {
 	return 0
 }
 
-func detectTraceable(spec *ebpf.CollectionSpec, addrs []uintptr) ([]uintptr, error) {
+func detectTraceable(spec *ebpf.CollectionSpec, addrs []uintptr) ([]bool, []uint64, error) {
 	spec = spec.Copy()
 
 	var addresses [AddrCap]uint64
@@ -34,23 +34,22 @@ func detectTraceable(spec *ebpf.CollectionSpec, addrs []uintptr) ([]uintptr, err
 		addresses[i] = uint64(addr)
 	}
 	if err := spec.Variables["addrs"].Set(addresses); err != nil {
-		return nil, fmt.Errorf("failed to set addrs: %w", err)
+		return nil, nil, fmt.Errorf("failed to set addrs: %w", err)
 	}
 	if err := spec.Variables["nr_addrs"].Set(uint32(len(addrs))); err != nil {
-		return nil, fmt.Errorf("failed to set nr_addrs: %w", err)
+		return nil, nil, fmt.Errorf("failed to set nr_addrs: %w", err)
 	}
 	if err := spec.Variables["has_endbr"].Set(uint32(b2i(hasEndbr))); err != nil {
-		return nil, fmt.Errorf("failed to set has_endbr: %w", err)
+		return nil, nil, fmt.Errorf("failed to set has_endbr: %w", err)
+	}
+	if err := spec.Variables["tramp_jmp"].Set(uint32(b2i(trampJmpMode))); err != nil {
+		return nil, nil, fmt.Errorf("failed to set tramp_jmp: %w", err)
 	}
 
 	spec.Programs["detect"].AttachTo = sysNanosleepSymbol
-	coll, err := ebpf.NewCollectionWithOptions(spec, ebpf.CollectionOptions{
-		Programs: ebpf.ProgramOptions{
-			LogDisabled: true,
-		},
-	})
+	coll, err := ebpf.NewCollection(spec)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create bpf collection: %w", err)
+		return nil, nil, fmt.Errorf("failed to create bpf collection: %w", err)
 	}
 	defer coll.Close()
 
@@ -60,7 +59,7 @@ func detectTraceable(spec *ebpf.CollectionSpec, addrs []uintptr) ([]uintptr, err
 		AttachType: ebpf.AttachTraceFEntry,
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to fentry nanosleep: %w", err)
+		return nil, nil, fmt.Errorf("failed to fentry nanosleep: %w", err)
 	}
 	defer l.Close()
 
@@ -68,25 +67,23 @@ func detectTraceable(spec *ebpf.CollectionSpec, addrs []uintptr) ([]uintptr, err
 
 	var run bool
 	if err := coll.Variables["run"].Get(&run); err != nil {
-		return nil, fmt.Errorf("failed to get run: %w", err)
+		return nil, nil, fmt.Errorf("failed to get run: %w", err)
 	}
 	if !run {
-		return nil, errors.New("traceable detection was not triggered")
+		return nil, nil, errors.New("traceable detection was not triggered")
+	}
+
+	var tramps [AddrCap]uint64
+	if err := coll.Variables["tramps"].Get(&tramps); err != nil {
+		return nil, nil, fmt.Errorf("failed to get tramps: %w", err)
 	}
 
 	var traceables [AddrCap]bool
 	if err := coll.Variables["traceables"].Get(&traceables); err != nil {
-		return nil, fmt.Errorf("failed to get traceables: %w", err)
+		return nil, nil, fmt.Errorf("failed to get traceables: %w", err)
 	}
 
-	var nontraceables []uintptr
-	for i := 0; i < len(addrs); i++ {
-		if !traceables[i] {
-			nontraceables = append(nontraceables, addrs[i])
-		}
-	}
-
-	return nontraceables, nil
+	return traceables[:len(addrs)], tramps[:len(addrs)], nil
 }
 
 func detectTraceables(kfuncs KFuncs, silent bool) (KFuncs, error) {
@@ -108,14 +105,16 @@ func detectTraceables(kfuncs KFuncs, silent bool) (KFuncs, error) {
 			addrs = nil
 		}
 
-		nontraceables, err := detectTraceable(spec, detect)
+		traceables, _, err := detectTraceable(spec, detect)
 		if err != nil {
 			return kfuncs, fmt.Errorf("failed to detect traceable: %w", err)
 		}
 
-		for _, nt := range nontraceables {
-			verboseLogIf(!silent, "Skip non-traceable kernel function %s", kfuncs[nt].Ksym.name)
-			delete(kfuncs, nt)
+		for i, t := range traceables {
+			if !t {
+				verboseLogIf(!silent, "Skip non-traceable kernel function %s", kfuncs[detect[i]].Ksym.name)
+				delete(kfuncs, detect[i])
+			}
 		}
 	}
 

internal/bpfsnoop/bpf_tracing_graph.go
@@ -7,14 +7,18 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/btf"
 	"github.com/cilium/ebpf/link"
+	"golang.org/x/exp/maps"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sys/unix"
 
 	"github.com/bpfsnoop/bpfsnoop/internal/bpf"
+	"github.com/bpfsnoop/bpfsnoop/internal/slicex"
+	"github.com/bpfsnoop/gapstone"
 )
 
 const (
@@ -134,7 +138,6 @@ func (t *bpfTracing) traceGraph(spec *ebpf.CollectionSpec,
 	})
 	t.llock.Unlock()
 
-	graph.traced = true
 	return nil
 }
 
@@ -267,6 +270,10 @@ func (t *bpfTracing) traceGraphs(reusedMaps map[string]*ebpf.Map, graphs FuncGra
 		return fmt.Errorf("failed to populate fgraph tracee IPs map: %w", err)
 	}
 
+	if err := t.populateFgraphTrampolineIPs(traceeIPs, graphs); err != nil {
+		return fmt.Errorf("failed to populate fgraph trampoline IPs: %w", err)
+	}
+
 	return nil
 }
 
@@ -306,6 +313,108 @@ func (t *bpfTracing) populateFgraphTraceeIPs(ips *ebpf.Map, graphs FuncGraphs) e
 	}
 	if _, err := ips.BatchUpdate(keys, vals, nil); err != nil {
 		return fmt.Errorf("failed to update fgraph tracee IPs map: %w", err)
+	}
+
+	return nil
+}
+
+func probeTrampAddrs(graphs FuncGraphs, silent bool) ([]uint64, []uint64, error) {
+	spec, err := bpf.LoadTraceable()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to load traceable bpf spec: %w", err)
+	}
+
+	addrs := maps.Keys(graphs)
+	slices.Sort(addrs)
+
+	trampAddrs := make([]uint64, 0, len(addrs))
+	kfuncAddrs := make([]uint64, 0, len(addrs))
+	for len(addrs) != 0 {
+		var detect []uintptr
+		if len(addrs) > AddrCap {
+			detect = slicex.U64sToUintptrs(addrs[:AddrCap])
+			addrs = addrs[AddrCap:]
+		} else {
+			detect = slicex.U64sToUintptrs(addrs)
+			addrs = nil
+		}
+
+		traceable, tramps, err := detectTraceable(spec, detect)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to detect traceable: %w", err)
+		}
+
+		for i := range detect {
+			if traceable[i] {
+				// map trampoline call-return addr to the original func
+				// addr, so that we can recognize them in stack
+				// unwinding.
+				trampAddrs = append(trampAddrs, tramps[i])
+				kfuncAddrs = append(kfuncAddrs, uint64(detect[i]))
+			}
+		}
+	}
+
+	return trampAddrs, kfuncAddrs, nil
+}
+
+func (t *bpfTracing) extractTrampolineIPs(addr uint64, engine *gapstone.Engine, ksyms *Kallsyms) ([]uint64, error) {
+	insts, err := disasmKfuncAt(addr, 0, ksyms, engine)
+	if err != nil {
+		return nil, fmt.Errorf("failed to disassemble trampoline at %#x: %w", addr, err)
+	}
+
+	ips, pc := make([]uint64, 0, 4), addr
+	for i := range insts {
+		pc += uint64(insts[i].Size)
+		if insts[i].Bytes[0] == 0xE8 { // callq
+			ips = append(ips, pc)
+		}
+	}
+	return ips, nil
+}
+
+func (t *bpfTracing) populateFgraphTrampolineIPs(ips *ebpf.Map, graphs FuncGraphs) error {
+	if !trampJmpMode {
+		return nil
+	}
+
+	trampAddrs, kfuncAddrs, err := probeTrampAddrs(graphs, true)
+	if err != nil {
+		return fmt.Errorf("failed to probe trampoline addresses: %w", err)
+	}
+
+	ksyms, err := NewKallsyms()
+	if err != nil {
+		return fmt.Errorf("failed to read kallsyms: %w", err)
+	}
+
+	engine, err := createGapstoneEngine()
+	if err != nil {
+		return fmt.Errorf("failed to create gapstone engine: %w", err)
+	}
+	defer engine.Close()
+
+	trampIPs := make(map[uint64]uint64, len(trampAddrs)*4)
+	for i, addr := range trampAddrs {
+		ips, err := t.extractTrampolineIPs(addr, engine, ksyms)
+		if err != nil {
+			return fmt.Errorf("failed to extract trampoline IPs: %w", err)
+		}
+
+		for _, ip := range ips {
+			trampIPs[ip] = kfuncAddrs[i]
+		}
+	}
+
+	keys := make([]uint64, 0, len(trampIPs))
+	vals := make([]uint64, 0, len(trampIPs))
+	for traceeIP, funcIP := range trampIPs {
+		keys = append(keys, traceeIP)
+		vals = append(vals, funcIP)
+	}
+	if _, err := ips.BatchUpdate(keys, vals, nil); err != nil {
+		return fmt.Errorf("failed to update fgraph trampoline IPs to bpf map: %w", err)
 	}
 
 	return nil

internal/bpfsnoop/disasm.go
@@ -55,7 +55,7 @@ func guessBytes(kaddr uintptr, ks *Kallsyms, bytes uint) uint {
 		return 4096 // limit to 4KiB
 	}
 
-	return uint(nxt.addr) - uint(kaddr)
+	return min(uint(nxt.addr)-uint(kaddr), 4096)
 }
 
 func trimTailingInsns(b []byte) []byte {

internal/bpfsnoop/func_graph.go
@@ -110,7 +110,6 @@ type FuncGraph struct {
 	ArgsEnSz int
 	ArgsExSz int
 	bytes    uint
-	traced   bool
 }
 
 type FuncGraphs map[uint64]*FuncGraph // key is the func IP
@@ -233,8 +232,4 @@ func FindGraphFuncs(ctx context.Context, flags *Flags, kfuncs KFuncs, bprogs *bp
 		}
 	}
 	return parser.graphs, nil
-}
-
-func FgraphExceedMaxDepth(flags *Flags, graphs FuncGraphs) bool {
-	return len(graphs) > 0 && flags.fgraphDepth > 10 && !tailcallInfo.supportTailcallInBpf2bpf
 }

internal/bpfsnoop/func_graph_proto.go
@@ -81,12 +81,12 @@ func detectKfuncTraceable(fnName string, ksyms *Kallsyms, fexit, silent bool) (b
 		return false, fmt.Errorf("failed to load traceable spec: %w", err)
 	}
 
-	nontraceables, err := detectTraceable(spec, []uintptr{uintptr(ksym.addr)})
+	traceables, _, err := detectTraceable(spec, []uintptr{uintptr(ksym.addr)})
 	if err != nil {
 		return false, fmt.Errorf("failed to detect traceable functions: %w", err)
 	}
 
-	return !slices.Contains(nontraceables, uintptr(ksym.addr)), nil
+	return traceables[0], nil
 }
 
 func (p *fgraphProto) checkTraceable(fnName string) bool {

commit ce752fded1d5962ee28386b5461daaa2b0eacbe4

bpf: Fix traceable
Fix the issue "misaligned stack access off 0+-12+0 size 8" again, similar
to commit 077a0c7fc990.

The issue was introduced by 01c5cf6dfa70.

Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

bpf/traceable.c
@@ -27,15 +27,16 @@ probe_addr_info(int i)
 
 #if defined(bpf_target_x86)
     static const u64 nop5 = 0x0000441F0F;
-    u64 a, b;
+    u64 a, b, c;
 
     ptr = has_endbr ? buff + 4 : buff;
-    if (tramp_jmp && ptr[0] == 0xE9 /* jmp */)
-        tramps[i] = addr + (has_endbr ? 4 : 0) + 5 + (__s32) (((*(u64 *) ptr) >> 8) & 0xFFFFFFFF);
     /* Avoid 'misaligned stack access off 0+-12+0 size 8' */
     a = *(u32 *) ptr;
     b = *(u8 *) (ptr + 4);
-    traceable = (b<<32 | a) == nop5 /* nop5 */ || ptr[0] == 0xE8 /* callq */ ||
+    c = (b << 32) | a;
+    if (tramp_jmp && ptr[0] == 0xE9 /* jmp */)
+        tramps[i] = addr + (has_endbr ? 4 : 0) + 5 + (__s32) ((c >> 8) & 0xFFFFFFFF);
+    traceable = c == nop5 /* nop5 */ || ptr[0] == 0xE8 /* callq */ ||
            /* 373f2f44c300 ("bpf,x86: adjust the "jmp" mode for bpf trampoline")
             * since 6.19 kernel.
             */